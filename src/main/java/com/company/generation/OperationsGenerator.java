11bc3368063f766807050cb0196904be5b947071
MrDok <gonshed@gmail.com>
2017-03-20 00:05:06 +0300

    implemented OperationsGenerator

--- src/main/java/com/company/generation/OperationsGenerator.java
+++ src/main/java/com/company/generation/OperationsGenerator.java
@@ -5,22 +5,34 @@ package com.company.generation;
  */
 public class OperationsGenerator implements Generator{
 
+    private int currentValue;
+    private int maxCombinationCount;
+    private int operationCount;
+
+    public OperationsGenerator(int size, int operationCount){
+        if (size > 0){
+            this.maxCombinationCount = 4 * (size - 1);
+            this.operationCount = operationCount;
+        }else{
+            maxCombinationCount = 0;
+        }
+    }
+
     @Override
     public String next(){
-        return null;
+        return Integer.toString(currentValue++, operationCount);
     }
 
     @Override
     public boolean hasNext(){
-        return false;
+        return (currentValue < maxCombinationCount);
     }
 
     public static void main(String[] args){
-        System.out.println("start");
-        for (int i = 0; i < 4096; i++){
-            Integer.toString(i, 4);
-        }
+        OperationsGenerator generator = new OperationsGenerator(3, 2);
 
-        System.out.println("finish");
+        while (generator.hasNext()){
+            System.out.println(generator.next());
+        }
     }
 }
